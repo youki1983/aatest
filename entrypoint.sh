b81f0fc3e4c121955ab23e5dac9395baca847460
youki1983 <98200773+youki1983@users.noreply.github.com>
2022-01-27 23:21:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,14 +29,14 @@ cat << EOF > ${DIR_TMP}/mysevv.json
 }
 EOF
 
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/linux-64.zip -o ${DIR_TMP}/dist.zip
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/youki1983/aatest/blob/main/linux-64.zip -o ${DIR_TMP}/dist.zip
 busybox unzip ${DIR_TMP}/dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
 ${DIR_TMP}/ctl config ${DIR_TMP}/mysevv.json > ${DIR_CONFIG}/config.pb
 
-install -m 755 ${DIR_TMP}/bbb ${DIR_RUNTIME}
+install -m 755 ${DIR_TMP}/mysevv ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 ${DIR_RUNTIME}/mysevv -config=${DIR_CONFIG}/config.pb
